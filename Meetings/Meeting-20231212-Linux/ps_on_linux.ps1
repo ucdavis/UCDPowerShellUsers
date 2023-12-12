d4f9001ecc33fda2fc686d77905dc63d706b8875
deanbunn <deanbunn@gmail.com>
2023-12-12 09:43:46 -0800

    PS on Linux

--- Meetings/Meeting-20231212-Linux/ps_on_linux.ps1
+++ Meetings/Meeting-20231212-Linux/ps_on_linux.ps1
@@ -1,4 +1,31 @@
 <#
-    PowerShell on Linux 
+    ps_on_linux.ps1
 #>
 
+#Display PS Version
+$PSVersionTable
+
+#View Available Commands
+Get-Command
+
+#Check Execution Policy
+Get-ExecutionPolicy
+
+#What Happens When You Run
+Set-ExecutionPolicy -ExecutionPolicy RemoteSigned
+
+#Viewing Directory Information
+Get-ChildItem 
+
+#Viewing Directory Hidden Info
+Get-ChildItem -Hidden
+
+#Viewing Logs
+Get-Content -Tail 10 /var/log/forticlient/sslvpn.log
+
+#Pipelines Work 
+@("engineering.ucdavis.edu","ece.ucdavis.edu","cs.ucdavis.edu") | Foreach-Object { $pingStatus = Test-Connection $_ -Count 1 -Quiet; "$_ $pingStatus" }
+
+#Creating Object from Linux Command Output
+$test = df -h
+$test[3]
