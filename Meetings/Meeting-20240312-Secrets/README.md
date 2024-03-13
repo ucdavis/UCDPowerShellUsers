d90e25480d305a7a3992b7d399bcb008b591120a
deanbunn <deanbunn@gmail.com>
2024-03-13 06:35:33 -0700

    20240313_0635hrs Secrets Management

---
+++ Meetings/Meeting-20240312-Secrets/README.md
@@ -0,0 +1,8 @@
+## Secret Management 
+
+### Resource Links
+
+[Overview of the SecretManagement and SecretStore modules](https://learn.microsoft.com/en-us/powershell/utility-modules/secretmanagement/overview)
+
+[Use the SecretStore in automation](https://learn.microsoft.com/en-us/powershell/utility-modules/secretmanagement/how-to/using-secrets-in-automation)
+
---
+++ Meetings/Meeting-20240312-Secrets/secrets_management.ps1
@@ -0,0 +1,53 @@
+<#
+    Title: secrets_management.ps1
+    Authors: Dean Bunn and Ben Clark
+    Last Edit: 2024-03-12
+#>
+
+#Stopping an Accidental Run
+exit
+
+#Find the SecretManagement Modules
+Find-Module -Tag SecretManagement
+
+#Install SecretManagement Modules
+Install-Module Microsoft.PowerShell.SecretManagement -Repository PSGallery
+Install-Module Microsoft.PowerShell.SecretStore -Repository PSGallery
+
+#Import SecretManagement Modules
+Import-Module Microsoft.PowerShell.SecretManagement
+Import-Module Microsoft.PowerShell.SecretStore
+
+#Setup or View Secret Store Configuration
+Get-SecretStoreConfiguration
+
+#Create Default Secret Vault
+Register-SecretVault -Name "NFLTeams" -ModuleName Microsoft.PowerShell.SecretStore -DefaultVault
+
+#View Vault Information
+Get-SecretVault
+
+#Add Secrets
+Set-Secret -Name "Raiders-QB" -Secret "Gardner Minshew"
+Set-Secret -Name "Niners-QB" -Secret "Brock Purdy"
+Set-Secret -Name "Chiefs-QB" -Secret "Kermit Mahomes"
+
+#View Secret
+Get-Secret -Name "Niners-QB" -AsPlainText
+
+#Storing Secrets Configuration General Password
+$credential = Get-Credential -UserName 'SecureStore'
+$securePasswordPath = 'C:\db\passwd.xml'
+$credential.Password |  Export-Clixml -Path $securePasswordPath
+
+#Change Secrets Store to Have No Interaction and a Longer Timeout
+Set-SecretStoreConfiguration -Authentication 'Password' -PasswordTimeout 3600 -Interaction 'None'
+
+#Now that Secrets Store is Locked. Open it with Stored Encrypted Password
+$password = Import-CliXml -Path $securePasswordPath
+Unlock-SecretStore -Password $password
+
+#Pull Secret and Assign to Variable
+$raidersQB = Get-Secret -Name 'Raiders-QB' -AsPlainText
+Write-Output $raidersQB
+
