e4244257fa88b0fe2c6da79135e020b1dca761ea
deanbunn <deanbunn@gmail.com>
2024-12-04 15:39:39 -0800

    20241204_1538hrs Slack msg section

--- Meetings/Meeting-20241210-Slack/slack_alerts.ps1
+++ Meetings/Meeting-20241210-Slack/slack_alerts.ps1
@@ -14,21 +14,81 @@ $SlackAPIInfo.dev_channel_path = Get-Secret -Name "Slack-Webhook-Dev" -AsPlainTe
 #Quick Check to Make Sure Webhook Values Pulled From Vault
 if([string]::IsNullOrEmpty($SlackAPIInfo.webhook_base) -eq $false -and [string]::IsNullOrEmpty($SlackAPIInfo.dev_channel_path) -eq $false)
 {
+
     #Var for Dev Channel URI 
     $uriDevChannel = $SlackAPIInfo.webhook_base + $SlackAPIInfo.dev_channel_path;
 
-    #Custom Object for Slack API Message Body
-    $cstPostBody = New-Object PSObject -Property (@{text="";});
+    <#
+    #Custom Object for Basic Slack API Message Body
+    $cstPostBodyBasic = New-Object PSObject -Property (@{text="";});
 
     #Set Message Text
-    $cstPostBody.text = "Whooty whooo";
+    $cstPostBodyBasic.text = "Whooty whooo";
 
-    #Convert Post Body to Json Object
-    $jsonPostBody = $cstPostBody | ConvertTo-Json -Compress;
+    #Convert Basic Post Body Object to Json
+    $jsonPostBodyBasic = $cstPostBodyBasic | ConvertTo-Json -Compress;
 
-    #Make API call to AWS
-    $slackAPICallStatus = Invoke-RestMethod -Uri $uriDevChannel -Method Post -Body $jsonPostBody -ContentType "application/json" -TimeoutSec 240;
+    #Make API call to Slack
+    $slackAPIBasicCallStatus = Invoke-RestMethod -Uri $uriDevChannel -Method Post -Body $jsonPostBodyBasic -ContentType "application/json" -TimeoutSec 240;
 
-    $slackAPICallStatus;
+    $slackAPIBasicCallStatus;
+    #>
+
+    #Array of Server Names to Ping Check
+    $arrServers = @("coe-it-wins22","coe-it-app22","coe-it-sql22");
+
+    #Var for Json Post Servers 
+    [string]$jsonPostServers = "{""blocks"": [{""type"": ""header"",""text"": {""type"": ""plain_text"",""text"": ""Server Availability Status"",""emoji"": true }},";
+
+    #Var for Server Ping Status Json
+    [string]$jsonSrvStatus = "";
+
+    foreach($uServer in $arrServers)
+    {
+
+       #Var for Ping Count
+       [int32]$pingCntChecks = 4;
+
+       #Var for Max Ping Failure
+       [int32]$pingMaxFails = 2;
+
+       #Var for Ping Fails 
+       [int32]$pingFails = 0;
+
+       #Ping Server Four Times and Check Results
+       $pingResults = Test-Connection -TargetName $uServer -Count $pingCntChecks;
+
+       foreach($pingResult in $pingResults)
+       {
+
+           if($pingResult.Status.ToString().ToLower() -ne "success")
+           {
+                $pingFails++;
+           }
+
+       }
+
+       if($pingFails -ge $pingMaxFails)
+       {
+         #Add Failed Server Status Block to Json Server Status
+         $jsonSrvStatus += "{""type"": ""section"",""fields"": [{""type"": ""mrkdwn"",""text"": "":ghost:""},{""type"": ""mrkdwn"",""text"": """ + $uServer + """ }]},";
+
+       }
+       else 
+       {
+         #Add Happy Server Status Block to Json Server Status
+         $jsonSrvStatus += "{""type"": ""section"",""fields"": [{""type"": ""mrkdwn"",""text"": "":smiley:""},{""type"": ""mrkdwn"",""text"": """ + $uServer + """ }]},";
+       }
+     
+    }
+
+    #Remove Trailing Comma
+    $jsonPostServers += $jsonSrvStatus.TrimEnd(",");
+    $jsonPostServers += "]}";
+
+    #Make API call to Slack
+    $slackAPIAdvCallStatus = Invoke-RestMethod -Uri $uriDevChannel -Method Post -Body $jsonPostServers -ContentType "application/json" -TimeoutSec 240;
+
+    $slackAPIAdvCallStatus;
 
 }#End of SlackAPIInfo Null\Empty Checks
