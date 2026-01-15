4112c4fc2dfc5d9593113bf22b3a5cc90497163b
deanbunn <deanbunn@gmail.com>
2026-01-15 14:27:08 -0800

    20260115_1426hrs Graph Directory Management

--- Meetings/Meeting-20260210-MicrosoftGraphDirectoryManagement/README.md
+++ Meetings/Meeting-20260210-MicrosoftGraphDirectoryManagement/README.md
@@ -1,68 +1,96 @@
-<#
-    Title: microsoft_graph_directory_management.ps1
-    Authors: Dean Bunn and Justin Earley
-    Last Edit: 2026-01-15
-#>
+## Micrsoft Graph Directory Management
 
-#Stopping an Accidental Run
-Exit;
+Part 4 of series on Microsoft Graph PowerShell SDK usage. This session will cover directory management module.
 
-#Install Required Modules
+### Resource Links
+
+[Microsoft Azure Portal](https://portal.azure.com/#allservices)
+
+[Microsoft.Graph.Identity.DirectoryManagement Module](https://learn.microsoft.com/en-us/powershell/module/microsoft.graph.identity.directorymanagement)
+
+[Microsoft Graph Explorer](https://developer.microsoft.com/en-us/graph/graph-explorer)
+
+[Intune / Entra Terminology UCD KB](https://kb.ucdavis.edu/?id=10695)
+
+
+### Module Installation Commands
+Install Required Modules
+```powershell
 Install-Module -Name Microsoft.Graph.Authentication,
                      Microsoft.Graph.Users,
                      Microsoft.Graph.Groups,
                      Microsoft.Graph.Identity.DirectoryManagement -Scope CurrentUser -Repository PSGallery -Force
-
-#View Installed Modules
-Get-InstalledModule
-
-#View Available Commands in a Module
+```
+View Installed Modules
+```powershell
+Get-InstalledModule 
+```
+View Available Commands in a Module
+```powershell
 Get-Command -Module Microsoft.Graph.Identity.DirectoryManagement
-
-#Import Required Modules
+```
+### Module Import and Connection Commands
+Import Required Modules
+```powershell
 Import-Module Microsoft.Graph.Authentication,Microsoft.Graph.Users,Microsoft.Graph.Groups,Microsoft.Graph.Identity.DirectoryManagement
-
-#Connect to Microsoft Graph
+```
+Connect to Microsoft Graph
+```powershell
 Connect-MgGraph
-
-#Disconnect Microsoft Graph Session
+```
+Disconnect Microsoft Graph Session
+```powershell
 Disconnect-MgGraph
-
-#View All Federated and Managed Domains
+```
+### Domain Commands
+View All Federated and Managed Domains
+```powershell
 Get-MgDomain -All | Sort-Object Id
-
-#View Information about Specific Domain
+```
+View Information about Specific Domain
+```powershell
 Get-MgDomain -DomainId ece.ucdavis.edu | Format-List
-
-#View Root Domain on Specific Domain
+```
+View Root Domain on Specific Domain
+```powershell
 Get-MgDomainRootDomain -DomainId dbs.ucdavis.edu | Format-List
-
-#Retrieve Organization Information Including Last Sync Information
+```
+Retrieve Organization Information Including Last Sync Information
+```powershell
 Get-MgOrganization | Select-Object Id,DisplayName,OnPremisesLastSyncDateTime,OnPremisesSyncEnabled | Format-Table -AutoSize
-
-#Retrieve Additional Information on Verified Domains
+```
+Retrieve Additional Information on Verified Domains
+```powershell
 (Get-MgOrganization).VerifiedDomains | Select-Object Name,Type,IsInitial,IsDefault,Capabilities| Sort-Object Name | Format-Table -AutoSize 
-
-#Get Federated Domains Configuration Information
+```
+Get Federated Domains Configuration Information
+```powershell
 (Get-MgOrganization).VerifiedDomains | Foreach-Object { if($_.Type -eq "Federated"){ Get-MgDomainFederationConfiguration -DomainId $_.Name | Format-List } }
-
-#View Office365 Contacts
+```
+### Contact Commands
+View Office365 Contacts
+```powershell
 # Get-MgContact -All #Will take a very long time to complete. Not recommended
-
-#Search for Contact by Mail Property
+```
+Search for Contact by Mail Property
+```powershell
 Get-MgContact -Search '"Mail:powershell@ucdavis.edu"' -ConsistencyLevel eventual -All | Format-List
-
-#Search for Contact by a Specific Proxy Address
+```
+Search for Contact by a Specific Proxy Address
+```powershell
 Get-MgContact -Filter "proxyAddresses/any(p:p eq 'smtp:powershell-request@lists.ucdavis.edu')" -ConsistencyLevel eventual -All | Format-List
-
-#Find All Contacts with Addresses Starting with a Specific Term
+```
+Find All Contacts with Addresses Starting with a Specific Term
+```powershell
 Get-MgContact -Filter "proxyAddresses/any(p:startswith(p,'smtp:powershell'))" -ConsistencyLevel eventual -All
 #Or
 Get-MgContact -Filter "proxyAddresses/any(p:startswith(p,'SMTP:engr'))" -ConsistencyLevel eventual -All
-
-#Find All Contacts from a Specific Domain
+```
+Find All Contacts from a Specific Domain
+```powershell
 Get-MgContact -Filter "proxyAddresses/any(p:endswith(p,'@hotmail.com'))" -ConsistencyLevel eventual -All
-
+```
+```powershell
 # Any Expression Format
 # any(x: x eq 'value')
 #     ^  ^
@@ -76,44 +104,56 @@ Get-MgContact -Filter "proxyAddresses/any(p:endswith(p,'@hotmail.com'))" -Consis
 # collection/any(x:endswith(x,'value'))
 #
 # Works Only on Properties that are Collections
+```
 
-#Retrieve All Devices
+### Device Commands
+Retrieve All Devices
+```powershell
 Get-MgDevice -ConsistencyLevel eventual -All
-
-#Get Devices with a Specific Naming Structure
+```
+Get Devices with a Specific Naming Structure
+```powershell
 Get-MgDevice -Filter "startswith(displayName,'coe-mae-')" -ConsistencyLevel eventual -All | `
  Select-Object Id,DisplayName,OperatingSystem,OperatingSystemVersion,TrustType,IsManaged,IsCompliant,RegistrationDateTime,ApproximateLastSignInDateTime | `
  Format-Table -AutoSize
-
-#Get Devices with a Specific Naming Prefix and have Signed In within the Last Two Months
+```
+Get Devices with a Specific Naming Prefix and have Signed In within the Last Two Months
+```powershell
 Get-MgDevice -Filter "startswith(displayName,'coe-mae-')" -ConsistencyLevel eventual -All | `
  Where-Object { $_.ApproximateLastSignInDateTime -ge (Get-Date).AddMonths(-2) } | `
  Select-Object Id,DisplayName,ApproximateLastSignInDateTime | Format-Table -AutoSize
-
+```
+```powershell
 #The Returned "Id" is the "DeviceID" when Querying for Systems. 
-
-#Get Individual Device Information by "DeviceId" which is Really "Id"
+```
+Get Individual Device Information by "DeviceId" which is Really "Id"
+```powershell
 Get-MgDevice -DeviceId "3229f204-e0a3-47e5-8231-8d22d762f718" | Format-List
-
-#Get Individual Device Information by Actual DeviceId 
+```
+Get Individual Device Information by Actual DeviceId
+```powershell
 Get-MgDeviceByDeviceId -DeviceId "22d1b65f-a7e7-4ef1-993a-c84b10b0bd18" | Format-List
-
-#Get Individual Device Membership
+```
+Get Individual Device Membership
+```powershell
 Get-MgDeviceMemberOf -DeviceId "db555ea7-69dc-4cda-8563-66505a2f4b8d" | Foreach-Object { $_.AdditionalProperties; write-output "`n`n"; }
 #Or by Device Display Name
 Get-MgDeviceMemberOf -DeviceId (Get-MgDevice -Filter "displayName eq 'COE-J238H03'").Id | Foreach-Object { $_.AdditionalProperties; write-output "`n`n"; }
-
-#Retrieve Registered Owner of Device
+```
+Retrieve Registered Owner of Device
+```powershell
 Get-MgDeviceRegisteredOwner -DeviceId "db555ea7-69dc-4cda-8563-66505a2f4b8d" | Foreach-Object { $_.AdditionalProperties; }
 #Or by Device Display Name 
 Get-MgDeviceRegisteredOwner -DeviceId (Get-MgDevice -Filter "displayName eq 'COE-J238H03'").Id | Foreach-Object { $_.AdditionalProperties; }
-
-#Retrieve Registered User of Device
+```
+Retrieve Registered User of Device
+```powershell
 Get-MgDeviceRegisteredUser -DeviceId "db555ea7-69dc-4cda-8563-66505a2f4b8d" | Foreach-Object { $_.AdditionalProperties; }
 #Or
 Get-MgDeviceRegisteredUser -DeviceId (Get-MgDevice -Filter "displayName eq 'COE-J238H03'").Id | Foreach-Object { $_.AdditionalProperties; }
-
-#Get Individual Device Membership Expanded Rules and Group Info
+```
+Get Individual Device Membership Expanded Rules and Group Info
+```powershell
 Get-MgDeviceMemberOf -DeviceId "db555ea7-69dc-4cda-8563-66505a2f4b8d" | `
     Foreach-Object { foreach($ap in $_.AdditionalProperties) {
                        if($ap['@odata.type'].ToString().Contains("microsoft.graph.group"))
@@ -125,4 +165,4 @@ Get-MgDeviceMemberOf -DeviceId "db555ea7-69dc-4cda-8563-66505a2f4b8d" | `
                             Get-MgGroup -Filter $grpFilter | Select-Object Id,DisplayName,MembershipRule
                        }
 } } | Format-Table -AutoSize -Wrap
-
+```
--- Meetings/Meeting-20260210-MicrosoftGraphDirectoryManagement/microsoft_graph_directory_management.ps1
+++ Meetings/Meeting-20260210-MicrosoftGraphDirectoryManagement/microsoft_graph_directory_management.ps1
@@ -1,96 +1,68 @@
-## Micrsoft Graph Directory Management
+<#
+    Title: microsoft_graph_directory_management.ps1
+    Authors: Dean Bunn and Justin Earley
+    Last Edit: 2026-02-10
+#>
 
-Part 4 of series on Microsoft Graph PowerShell SDK usage. This session will cover directory management module.
+#Stopping an Accidental Run
+Exit;
 
-### Resource Links
-
-[Microsoft Azure Portal](https://portal.azure.com/#allservices)
-
-[Microsoft.Graph.Identity.DirectoryManagement Module](https://learn.microsoft.com/en-us/powershell/module/microsoft.graph.identity.directorymanagement)
-
-[Microsoft Graph Explorer](https://developer.microsoft.com/en-us/graph/graph-explorer)
-
-[Intune / Entra Terminology UCD KB](https://kb.ucdavis.edu/?id=10695)
-
-
-### Module Installation Commands
-Install Required Modules
-```powershell
+#Install Required Modules
 Install-Module -Name Microsoft.Graph.Authentication,
                      Microsoft.Graph.Users,
                      Microsoft.Graph.Groups,
                      Microsoft.Graph.Identity.DirectoryManagement -Scope CurrentUser -Repository PSGallery -Force
-```
-View Installed Modules
-```powershell
-Get-InstalledModule 
-```
-View Available Commands in a Module
-```powershell
+
+#View Installed Modules
+Get-InstalledModule
+
+#View Available Commands in a Module
 Get-Command -Module Microsoft.Graph.Identity.DirectoryManagement
-```
-### Module Import and Connection Commands
-Import Required Modules
-```powershell
-Import-Module Microsoft.Graph.Authentication,Microsoft.Graph.Users,Microsoft.Graph.Groups,Microsoft.Graph.DeviceManagement
-```
-Connect to Microsoft Graph
-```powershell
+
+#Import Required Modules
+Import-Module Microsoft.Graph.Authentication,Microsoft.Graph.Users,Microsoft.Graph.Groups,Microsoft.Graph.Identity.DirectoryManagement
+
+#Connect to Microsoft Graph
 Connect-MgGraph
-```
-Disconnect Microsoft Graph Session
-```powershell
+
+#Disconnect Microsoft Graph Session
 Disconnect-MgGraph
-```
-### Domain Commands
-View All Federated and Managed Domains
-```powershell
+
+#View All Federated and Managed Domains
 Get-MgDomain -All | Sort-Object Id
-```
-View Information about Specific Domain
-```powershell
+
+#View Information about Specific Domain
 Get-MgDomain -DomainId ece.ucdavis.edu | Format-List
-```
-View Root Domain on Specific Domain
-```powershell
+
+#View Root Domain on Specific Domain
 Get-MgDomainRootDomain -DomainId dbs.ucdavis.edu | Format-List
-```
-Retrieve Organization Information Including Last Sync Information
-```powershell
+
+#Retrieve Organization Information Including Last Sync Information
 Get-MgOrganization | Select-Object Id,DisplayName,OnPremisesLastSyncDateTime,OnPremisesSyncEnabled | Format-Table -AutoSize
-```
-Retrieve Additional Information on Verified Domains
-```powershell
+
+#Retrieve Additional Information on Verified Domains
 (Get-MgOrganization).VerifiedDomains | Select-Object Name,Type,IsInitial,IsDefault,Capabilities| Sort-Object Name | Format-Table -AutoSize 
-```
-Get Federated Domains Configuration Information
-```powershell
+
+#Get Federated Domains Configuration Information
 (Get-MgOrganization).VerifiedDomains | Foreach-Object { if($_.Type -eq "Federated"){ Get-MgDomainFederationConfiguration -DomainId $_.Name | Format-List } }
-```
-### Contact Commands
-View Office365 Contacts
-```powershell
+
+#View Office365 Contacts
 # Get-MgContact -All #Will take a very long time to complete. Not recommended
-```
-Search for Contact by Mail Property
-```powershell
+
+#Search for Contact by Mail Property
 Get-MgContact -Search '"Mail:powershell@ucdavis.edu"' -ConsistencyLevel eventual -All | Format-List
-```
-Search for Contact by a Specific Proxy Address
-```powershell
+
+#Search for Contact by a Specific Proxy Address
 Get-MgContact -Filter "proxyAddresses/any(p:p eq 'smtp:powershell-request@lists.ucdavis.edu')" -ConsistencyLevel eventual -All | Format-List
-```
-Find All Contacts with Addresses Starting with a Specific Term
-```powershell
+
+#Find All Contacts with Addresses Starting with a Specific Term
 Get-MgContact -Filter "proxyAddresses/any(p:startswith(p,'smtp:powershell'))" -ConsistencyLevel eventual -All
 #Or
 Get-MgContact -Filter "proxyAddresses/any(p:startswith(p,'SMTP:engr'))" -ConsistencyLevel eventual -All
-```
-Find All Contacts from a Specific Domain
-```powershell
+
+#Find All Contacts from a Specific Domain
 Get-MgContact -Filter "proxyAddresses/any(p:endswith(p,'@hotmail.com'))" -ConsistencyLevel eventual -All
-```
-```powershell
+
 # Any Expression Format
 # any(x: x eq 'value')
 #     ^  ^
@@ -104,56 +76,44 @@ Get-MgContact -Filter "proxyAddresses/any(p:endswith(p,'@hotmail.com'))" -Consis
 # collection/any(x:endswith(x,'value'))
 #
 # Works Only on Properties that are Collections
-```
 
-### Device Commands
-Retrieve All Devices
-```powershell
+#Retrieve All Devices
 Get-MgDevice -ConsistencyLevel eventual -All
-```
-Get Devices with a Specific Naming Structure
-```powershell
+
+#Get Devices with a Specific Naming Structure
 Get-MgDevice -Filter "startswith(displayName,'coe-mae-')" -ConsistencyLevel eventual -All | `
  Select-Object Id,DisplayName,OperatingSystem,OperatingSystemVersion,TrustType,IsManaged,IsCompliant,RegistrationDateTime,ApproximateLastSignInDateTime | `
  Format-Table -AutoSize
-```
-Get Devices with a Specific Naming Prefix and have Signed In within the Last Two Months
-```powershell
+
+#Get Devices with a Specific Naming Prefix and have Signed In within the Last Two Months
 Get-MgDevice -Filter "startswith(displayName,'coe-mae-')" -ConsistencyLevel eventual -All | `
  Where-Object { $_.ApproximateLastSignInDateTime -ge (Get-Date).AddMonths(-2) } | `
  Select-Object Id,DisplayName,ApproximateLastSignInDateTime | Format-Table -AutoSize
-```
-```powershell
+
 #The Returned "Id" is the "DeviceID" when Querying for Systems. 
-```
-Get Individual Device Information by "DeviceId" which is Really "Id"
-```powershell
+
+#Get Individual Device Information by "DeviceId" which is Really "Id"
 Get-MgDevice -DeviceId "3229f204-e0a3-47e5-8231-8d22d762f718" | Format-List
-```
-Get Individual Device Information by Actual DeviceId
-```powershell
+
+#Get Individual Device Information by Actual DeviceId 
 Get-MgDeviceByDeviceId -DeviceId "22d1b65f-a7e7-4ef1-993a-c84b10b0bd18" | Format-List
-```
-Get Individual Device Membership
-```powershell
+
+#Get Individual Device Membership
 Get-MgDeviceMemberOf -DeviceId "db555ea7-69dc-4cda-8563-66505a2f4b8d" | Foreach-Object { $_.AdditionalProperties; write-output "`n`n"; }
 #Or by Device Display Name
 Get-MgDeviceMemberOf -DeviceId (Get-MgDevice -Filter "displayName eq 'COE-J238H03'").Id | Foreach-Object { $_.AdditionalProperties; write-output "`n`n"; }
-```
-Retrieve Registered Owner of Device
-```powershell
+
+#Retrieve Registered Owner of Device
 Get-MgDeviceRegisteredOwner -DeviceId "db555ea7-69dc-4cda-8563-66505a2f4b8d" | Foreach-Object { $_.AdditionalProperties; }
 #Or by Device Display Name 
 Get-MgDeviceRegisteredOwner -DeviceId (Get-MgDevice -Filter "displayName eq 'COE-J238H03'").Id | Foreach-Object { $_.AdditionalProperties; }
-```
-Retrieve Registered User of Device
-```powershell
+
+#Retrieve Registered User of Device
 Get-MgDeviceRegisteredUser -DeviceId "db555ea7-69dc-4cda-8563-66505a2f4b8d" | Foreach-Object { $_.AdditionalProperties; }
 #Or
 Get-MgDeviceRegisteredUser -DeviceId (Get-MgDevice -Filter "displayName eq 'COE-J238H03'").Id | Foreach-Object { $_.AdditionalProperties; }
-```
-Get Individual Device Membership Expanded Rules and Group Info
-```powershell
+
+#Get Individual Device Membership Expanded Rules and Group Info
 Get-MgDeviceMemberOf -DeviceId "db555ea7-69dc-4cda-8563-66505a2f4b8d" | `
     Foreach-Object { foreach($ap in $_.AdditionalProperties) {
                        if($ap['@odata.type'].ToString().Contains("microsoft.graph.group"))
@@ -165,4 +125,4 @@ Get-MgDeviceMemberOf -DeviceId "db555ea7-69dc-4cda-8563-66505a2f4b8d" | `
                             Get-MgGroup -Filter $grpFilter | Select-Object Id,DisplayName,MembershipRule
                        }
 } } | Format-Table -AutoSize -Wrap
-```
+
